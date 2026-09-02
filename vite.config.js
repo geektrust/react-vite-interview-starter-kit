cf79b6c2e04a85e815d715bcdbad6272e6e7facf
Himanshu Dubey <himanshu@geektrust.in>
2026-09-02 11:14:06 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "custom-urls",
+      configureServer(server) {
+        const original = server.printUrls.bind(server);
+        server.printUrls = () => {
+          original();
+          const port = server.config.server.port;
+          server.config.logger.info(
+            `  ➜  Preview: https://${process.env.CONTAINER_NAME}-${process.env.REGION}-${port}.userapp.qa-interview.geektrust.in`
+          );
+        };
+      },
+    },
+  ],
   server: {
     allowedHosts: true,
     hmr: {
-      clientPort: 443
-    }
-  }
-})
+      clientPort: 443,
+    },
+  },
+});
